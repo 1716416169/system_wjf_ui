6bdddbff48e800363bcf814df712d155805dc810
wjf <1716416169@qq.com>
2019-12-07 21:23:08 +0800

    ui

--- js/filter.js
+++ js/filter.js
@@ -1,4 +1,17 @@
 window.onload=function()//用window的onload事件，窗体加载完毕的时候
 {
-    console.log("666666666666666666")
+    var result=document.cookie
+   /* var start = str.indexOf("=");//获得字符串的开始位置
+    var result = str.substring(start+1,document.cookie.length);*/
+    console.log(result)
+    if (result=="user"){
+        console.log(result)
+    }else if(result=="manger") {
+        console.log(result)
+    }else {
+        alert("请登录")
+        window.location.href="login.html"
+
+    }
+
 }
--- js/url.js
+++ js/url.js
@@ -1,3 +1,3 @@
 var url="http://localhost:8080";
 var low="?page=1&size=30";
-var all_context=0;
+var all_context;
