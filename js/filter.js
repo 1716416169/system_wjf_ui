c5b0d9dd83b275a256444d4c02c368eec01f3d20
wjf <1716416169@qq.com>
2019-12-07 12:04:45 +0800

    ui

---
+++ js/filter.js
@@ -0,0 +1,4 @@
+window.onload=function()//用window的onload事件，窗体加载完毕的时候
+{
+    console.log("666666666666666666")
+}
--- js/url.js
+++ js/url.js
@@ -1,2 +1,3 @@
 var url="http://localhost:8080";
-var low="?page=1&size=30"
+var low="?page=1&size=30";
+var all_context=0;
